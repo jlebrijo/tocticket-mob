5c7ce8190e35bab198cc1b86e59f75ba28741858
Juan Lebrijo <juan@lebrijo.com>
2013-04-14 22:26:07 +0200

    #165 Mobile: QR scanning and App style

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,35 +1,24 @@
 var SERVER = "http://192.168.10.139:3000";
 var API = {
-    get_token: SERVER + "/api/v1/tokens.json",
+    get_token: SERVER + "/api/v1/tokens",
     events: function() {
-        return SERVER + "/api/v1/events.json?auth_token=" + token;
+        return SERVER + "/api/v1/events?auth_token=" + token;
+    },
+    check_ticket: function() {
+        return SERVER + "/api/v1/check_ticket?auth_token=" + token;
     }
 };
 
 var token = null;
 var selected_event = null;
 
-
-var scanCode = function () {
-    window.plugins.barcodeScanner.scan(
-        function (result) {
-            if (!result.cancelled) {
-
-            }
-
-
-            alert("Scanned Code: " + result.text
-                + ". Format: " + result.format
-                + ". Event: " + selected_event
-                + ". Cancelled: " + result.cancelled);
-        }, function (error) {
-            alert("Scan failed: " + error);
-        });
-};
+$(document).ready(function() {
+    render_partial("login", signing_in);
+});
 
 function render_partial(template, callback) {
     $.get("templates/_" + template + ".html", function(template) {
-        $("body").html(template);
+        $("#content").html(template);
         if (callback != undefined) {
             callback();
         }
@@ -40,18 +29,23 @@ function render_partial(template, callback) {
 var signing_in = function() {
     $("form").submit(function(e) {
         e.preventDefault();
-        credentials = "email=" + $("#username").val() + "&password=" + $("#password").val();
+        var credentials = "email=" + $("#username").val() + "&password=" + $("#password").val();
+        $("#loading").show();
         $.ajax({
             type: "POST",
             url: API.get_token,
             dataType: "json",
             data: credentials,
+            async: false,
             success: function (data){
+                $("#loading").hide();
                 token = data.token;
                 render_partial("events", listing_events);
             },
             error: function(xhr, ajaxOptions, thrownError) {
-                $("form .alert").html($.parseJSON(xhr.responseText).message).show();
+                $("#loading").hide();
+                var message = (xhr.responseText==undefined)? xhr.statusText : $.parseJSON(xhr.responseText).message;
+                $("form .alert-error").html(message).show();
             }
         });
     });
@@ -59,36 +53,77 @@ var signing_in = function() {
 
 var listing_events = function() {
     // List events
+    $("#loading").show();
     $.ajax({
         type: "GET",
         url: API.events(),
         dataType: "json",
+        async: false,
         success: function (data){
+            $("#loading").hide();
             $.each(data, function(i, event) {
-                $("#events").append()
+                var text = (event.name.length >30)? event.name.substr(0,30) + "...": event.name;
                 $('<button>', {
-                    text: event.name,
-                    class: 'btn',
+                    text: text,
+                    class: 'btn btn-block',
                     type: 'button',
                     'data-id': event.id
-                }).appendTo('#events')
+                }).appendTo('#events');
             })
         },
         error: function(xhr, ajaxOptions, thrownError) {
-            $("form .alert").html($.parseJSON(xhr.responseText).message).show();
+            $("#loading").hide();
+            var message = (xhr.responseText==undefined)? xhr.statusText : $.parseJSON(xhr.responseText).message;
+            $("#events .alert-error").html(message).show();
         }
     });
 
     // Click action for each event
     $(document).on("click", "#events .btn", function(e) {
         selected_event = $(this).data("id");
-        scanCode();
+        scanning_code();
     });
-}
-$(document).ready(function() {
-    render_partial("login", signing_in);
-});
+};
 
-
-
-
+var scanning_code = function () {
+    window.plugins.barcodeScanner.scan(
+        function (result) {
+            if (result.cancelled) {
+                render_partial("events", listing_events);
+            } else {
+                render_partial("ticket");
+                // Checking Ticket
+                var qr_array = result.text.split("/");
+                var ticket_req = "event=" + selected_event + "&ticket=" + qr_array[0] + "&key=" + qr_array[1]
+                $("#loading").show();
+                $.ajax({
+                    type: "POST",
+                    url: API.check_ticket(),
+                    dataType: "json",
+                    data: ticket_req,
+                    success: function (data){
+                        $("#loading").hide();
+                        ticket = data;
+                        $("#ticket .alert-success").html("Valid Ticket.").show();
+                        $('<p>', {
+                            text: "Attendees: " + ticket.attendees
+                        }).appendTo('#ticket .detail');
+                        $('<p>', {
+                            text: "Email: " + ticket.email
+                        }).appendTo('#ticket .detail');
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                        $("#loading").hide();
+                        var message = (xhr.responseText==undefined)? xhr.statusText : $.parseJSON(xhr.responseText).message;
+                        $("#ticket .alert-error").html(message).show();
+                    }
+                });
+                // Back to scan button
+                $(document).on("click", "#ticket .btn", function(e) {
+                    scanning_code();
+                });
+            }
+        }, function (error) {
+            alert("Scan failed: " + error);
+        });
+};
--- gen/tocticket/mob/R.java
+++ gen/tocticket/mob/R.java
@@ -146,7 +146,6 @@ public final class R {
         public static final int contents_phone=0x7f0a0020;
         public static final int contents_sms=0x7f0a0021;
         public static final int contents_text=0x7f0a0022;
-        public static final int hello=0x7f0a0079;
         public static final int history_clear_one_history_text=0x7f0a0024;
         public static final int history_clear_text=0x7f0a0023;
         public static final int history_email_title=0x7f0a0025;
