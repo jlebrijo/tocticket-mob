c150e13ad751fdddb0fd66aaa17a234cf397b67f
Juan Lebrijo <juan@lebrijo.com>
2013-04-14 05:46:04 +0200

    some refactorings

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,3 +1,11 @@
+var SERVER = "http://192.168.10.139:3000";
+var API = {
+    get_token: SERVER + "/api/v1/tokens.json"
+};
+
+var token = null;
+
+
 var scanCode = function () {
     window.plugins.barcodeScanner.scan(
         function (result) {
@@ -12,27 +20,31 @@ var scanCode = function () {
         }, function (error) {
             alert("Scan failed: " + error);
         });
-}
+};
 
-var token = null;
+function render_partial(template, callback) {
+    $.get("templates/_" + template + ".html", function(template) {
+        $("body").html(template);
+        if (callback != undefined) {
+            callback();
+        }
+    });
 
+};
 
 $(document).ready(function(){
-    $.get("templates/_login.html", function(template) {
-        $("body").html(template);
+    render_partial("login", function() {
         $("form").submit(function(e){
             e.preventDefault();
             credentials = "email=" + $("#username").val() + "&password=" + $("#password").val();
             $.ajax({
                 type: "POST",
-                url: "http://192.168.10.139:3000/api/v1/tokens.json",
+                url: API.get_token,
                 dataType: "json",
                 data: credentials,
                 success: function (data){
                     token = data.token;
-                    $.get("templates/_events.html", function(template) {
-                            $("body").html(template);
-                    });
+                    render_partial("events");
                 },
                 error: function(xhr, ajaxOptions, thrownError) {
                     $("form .alert").html($.parseJSON(xhr.responseText).message).show();
